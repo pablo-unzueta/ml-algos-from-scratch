24da9bb11405fb94b8aa8dd884d92bb8186bbf7d
pablo unzueta <punzu001@ucr.edu>
2024-12-13 10:46:52 -0800

    knn solved

--- unsupervised/knn/knn.py
+++ unsupervised/knn/knn.py
@@ -12,19 +12,33 @@ class kNN:
         self.data = X
         self.labels = y
 
-    def predict(self, query: np.ndarray):
-        # Use Euclidean distances and sort
-        print(self.data - query)
-        # indices = np.argsort(np.linalg.norm(self.data - query))
-        return self.labels[indices[: self.k]]
+    def predict(self, query: np.ndarray, method: str = "vectorized"):
+        method_type = ["vectorized", "loop"]
+        if method not in method_type:
+            raise ValueError(f"{method=}. Must be vectorized or loop!")
+        elif method == method_type[0]:
+            # Use Euclidean distances and get indices
+            indices = np.argsort(np.linalg.norm(self.data - query, axis=1))[: self.k]
+        else:
+            # Use for loop
+            dist = []
+            for point in self.data:
+                dist.append(np.sum((point - query) ** 2))
+            indices = sorted(range(len(dist)), key=lambda x: dist[x])
+        return self.labels[indices][: self.k]
 
 
-def plot_data(df: pd.DataFrame, path: Path, fig_name: str = "plot.png"):
+def plot_data(
+    X: np.ndarray,
+    labels: np.ndarray,
+    path: Path,
+    test_point: np.ndarray | None = None,
+    fig_name: str = "plot.png",
+):
     fig, ax = plt.subplots()
-    colors = pd.factorize(df["Class"])[0]
-    scatter_plot = ax.scatter(df["Weight(x2)"], df["Height(y2)"], c=colors)
-    ax.set_xlabel("Weight")
-    ax.set_ylabel("Height")
+    if test_point is not None:
+        ax.scatter(test_point[0], test_point[1], color="red", marker="*", s=100)
+    scatter_plot = ax.scatter(X[:, 0], X[:, 1], c=labels)
     plt.colorbar(scatter_plot)
     plt.savefig(path / fig_name)
 
@@ -36,9 +50,10 @@ if __name__ == "__main__":
     X = np.loadtxt(data_path / "data.txt", delimiter=",")
     y = np.loadtxt(data_path / "targets.txt")
 
-    k_nearest_neighbors = kNN(k=3)
+    k_nearest_neighbors = kNN(k=10)
     k_nearest_neighbors.fit(X, y)
 
-    x_test = np.array([1.0, 2.0])
-    neighbors = k_nearest_neighbors.predict(x_test)
+    x_test = np.array([1.0, -1.0])
+    plot_data(X=X, labels=y, path=data_path, test_point=x_test)
+    neighbors = k_nearest_neighbors.predict(x_test, method="vectorized")
     print(f"{neighbors=}")
